f362be82db3315ad756465c82b58bc6e52b45b19
skywie <festinelijah@gmail.com>
2026-07-03 23:25:06 +0800

    fix: resources, sort of task and color, code changes

--- client/src/pages/sections/ClassResources.jsx
+++ client/src/pages/sections/ClassResources.jsx
@@ -112,6 +112,7 @@ export default function ClassResources() {
       <header className="mt-3">
         <p className="font-bold text-[1.5rem] md:text-[1.7rem] font-[montserrat] leading-7">Class Resources</p>
         <p className="text-sm sm:text-base">Collaborate and exchange notes with your fellow blockmates.</p>
+        <p className="text-gray-400 text-xs sm:text-base mt-2">For the best viewing experience, we recommend uploading your files in <span className="font-bold">PNG, JPG/JPEG, or PDF</span> format.</p>
       </header>
 
       <div className="bg-white w-full mt-5 border border-gray-200 rounded-xl p-3 sm:p-4 flex flex-col">
@@ -228,10 +229,10 @@ export default function ClassResources() {
                 </div>
 
                 {error.general && (
-                  <p className="text-red-500 text-[.8rem] leading-4 font-bold my-1 text-center">{error.general}</p>
+                  <p className="text-red-500 text-[.8rem] leading-4 font-bold my-2 text-center">{error.general}</p>
                 )}
 
-                <div className="flex flex-col-reverse sm:flex-row justify-end gap-3 mt-5">
+                <div className="flex flex-col-reverse sm:flex-row justify-end gap-3 mt-4">
                   <Button
                     type="button"
                     onClick={handleClose}
