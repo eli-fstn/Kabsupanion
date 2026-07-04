ed03142980a1bd9bcfed8af2556eb4068c7939fe
skywie <festinelijah@gmail.com>
2026-07-05 01:01:13 +0800

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,6 @@
-console.log(import.meta.env.VITE_MAINTENANCE_MODE);
-console.log(import.meta.env);
-
 import AppRoutes from "./routes/AppRoutes.jsx";
-import Error503 from "./pages/errors/Error503.jsx";
 
 function App() {
-   if (import.meta.env.VITE_MAINTENANCE_MODE === "true") {
-    return <Error503 />;
-  }
   return <AppRoutes />;
 }
 
