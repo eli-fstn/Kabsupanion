64e16a04d30949ac19af42bca051da92c2a78b9b
skywie <festinelijah@gmail.com>
2026-07-24 11:54:03 +0800

    fix: when the student is still null due to lost connection, it will return a loading screen

--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -1,21 +1,16 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useUser } from "../context/userContext";
-import Error403 from "../pages/errors/Error403";
-import LoadingScreen from "../components/ui/LoadingScreen";
-
 const PrivateRoute = ({ requiredRole }) => {
   const token = localStorage.getItem("token");
   const { student, loading } = useUser();
 
   if (loading) return <LoadingScreen />;
 
-  if (!token) return <Navigate to="/" />;
+  if (!token) return <Navigate to="/" replace />;
 
-  if (requiredRole && student?.user?.role !== requiredRole) {
-    return <Error403 />
-  } 
- 
+  if (!student) return <LoadingScreen />;
+
+  if (requiredRole && student.user.role !== requiredRole) {
+    return <Error403 />;
+  }
+
   return <Outlet />;
 };
-
-export default PrivateRoute;
